c1164587dd8b871a1d0ca474eeaa78373ff9ef1e
nzxf <122195155+nzxf@users.noreply.github.com>
2024-11-11 22:53:52 +0700

    Add netlify adapter

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/world-of-drones_Cc98ZOmA.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./world-of-drones_wkY3a2qY.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
